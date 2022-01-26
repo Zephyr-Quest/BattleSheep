e3741d54db93634f3a55a85a88a0e0242a6706ec
EnguerrandMQT <enguerrand.marquant@student.junia.com>
2022-01-26 15:35:16 +0100

    Bim le serveur

--- index.js
+++ index.js
@@ -1,1 +1,28 @@
-//test
+const express = require('express');
+const app = express();
+const http = require('http').Server(app);
+const io = require('socket.io')(http);
+
+app.use(express.static(__dirname + '/front/'));
+
+app.get('/', (req, res) => {
+    res.sendFile(__dirname + '/front/index.html');
+});
+
+
+
+
+
+io.on('connection', (socket) => {
+    console.log("Connexion d'un utilisateur");
+
+    socket.on('disconnect', () =>{
+        console.log("Déconnexion d'un utilisateur");
+    });
+});
+
+
+
+http.listen(4200, () => {
+    console.log('Serveur lancé sur le port 4200');
+});
