c302dce5de69c184c81981e33e0e95a363e4ffda
Tom Mullier <tom.mullier@outlook.fr>
2022-02-02 17:34:08 +0100

    setter signup and getter

--- index.js
+++ index.js
@@ -102,4 +102,85 @@ const con = mysql.createConnection({
 con.connect(err => {
     if (err) throw err;
     else console.log("Connexion effectuée");
+
+    /**
+     * Insert user and password in table
+     *
+     * @param   {string}  user  username to insert
+     * @param   {string}  pass  password
+     *
+     * @return  {error}        return if error
+     */
+    function signUp(user,pass){
+        // Insert element
+        if(user=="" || pass==""){return;}
+        try {
+            const users={
+                username:user,
+                password:pass
+            }
+
+            sql = 'INSERT into users SET ?'
+            con.query(sql, users,(err,result)=>{
+                if (err) throw err;
+                console.log("1 element inserted")
+                console.log(result)
+            })
+        } catch (error) {
+            console.log(error);
+        }
+    }
+
+    /**
+     * look for user in table with correct password
+     *
+     * @param   {string}  user  username
+     * @param   {string}  pass  password
+     *
+     * @return  {Array}        array of users matching
+     */
+    function signIn(usr,pass){
+        let quer="SELECT * from users WHERE username='"+usr+"' AND password='"+pass+"'";
+        con.query(quer,(err,result)=>{
+            if(err) throw err;
+            if(result=="") console.log( "Utilisateur introuvable");
+            else {
+                console.log("Résultat trouvé : ")
+                console.log(result)
+                return result;
+            }
+        })
+    }
+
+    /**
+     * get user and password for a username given
+     *
+     * @param   {string}  user  username
+     *
+     * @return  {Array}       usr and pass
+     */
+    function getListFromUser(usr){
+        let quer="SELECT * from users WHERE username='"+usr+"'";
+        con.query(quer,(err,result)=>{
+            if(err) throw err;
+            console.log(result)
+            return result
+        })
+    }
+
+    /**
+     * get user and password for a id given
+     *
+     * @param   {string}  id  id
+     *
+     * @return  {Array}       usr and pass
+     */
+    function getListFromId(id){
+        let quer="SELECT * from users WHERE id='"+id+"'";
+        con.query(quer,(err,result)=>{
+            if(err) throw err;
+            console.log(result)
+            return result
+        })
+    }
 })
