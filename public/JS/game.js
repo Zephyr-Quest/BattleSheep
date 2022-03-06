04bbe605f0973d7dedc52b6ab1e3939c7391069e
Martin Delebecque <martin-delebecque@outlook.fr>
2022-03-06 18:45:03 +0100

    Add a spalsh screen

---
+++ public/JS/game.js
@@ -0,0 +1,46 @@
+import Game from "./ThreeJS/game.js";
+import SplashScreen from "./utils/SplashScreen.js";
+import { Config } from "./ThreeJS/config.js";
+import { HUD } from "./ThreeJS/gameplay/HUD.js";
+
+window.addEventListener("load", () => {
+    SplashScreen.start(() => {
+        // The splash screen is done
+        SplashScreen.hideScreen();
+        Game.init();
+    });
+
+    /* --------------------------------- Events --------------------------------- */
+
+    window.addEventListener("keyup", onKeyUp);
+});
+
+/**
+ * Callback of keyup event
+ * @param {KeyboardEvent} e The keyup event object
+ */
+function onKeyUp(e) {
+    if (e.code === 'Space') {
+        // Debug
+        HUD.hideAnnouncement();
+        setTimeout(HUD.showStartGrid, 1000);
+    } else if (e.key === 'f') {
+        const toFullscreen = document.querySelector("body");
+        if (toFullscreen.requestFullscreen)
+            toFullscreen.requestFullscreen();
+        else if (toFullscreen.webkitRequestFullscreen)
+            toFullscreen.webkitRequestFullscreen();
+        else if (toFullscreen.msRequestFullscreen)
+            toFullscreen.msRequestFullscreen();
+    } else {
+        // Check camera controls
+        for (const keyCode in Config.cameraPositions) {
+            if (!Config.cameraPositions.hasOwnProperty(keyCode) || e.key !== keyCode)
+                continue;
+
+            // Switch the camera position
+            const pos = Config.cameraPositions[keyCode];
+            Game.setCameraFromVector(pos);
+        }
+    }
+}
