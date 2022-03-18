05938a7a7e5a46ba2e8ca1f1cb036d677ee0353d
Maxime Declemy <maxime.declemy@student.junia.com>
2022-03-18 22:43:06 +0100

    comments

--- back/score.js
+++ back/score.js
@@ -1,5 +1,13 @@
 let score = 0;
 
+/**
+ * Function to calcul player score
+ *
+ * @param   {Number}  time             Time of the game in seconds
+ * @param   {Number}  nbNewSheepFound  Number of sheep found with the last shoot
+ *
+ * @return  {Number} score             Score of the player
+ */
 function calculScore(time, nbNewSheepFound) {
     score += nbNewSheepFound*100/sqrt(time);
 }
